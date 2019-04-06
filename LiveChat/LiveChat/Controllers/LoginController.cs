b31b7d56e350f7f0f39d1f2b75f7c2df38c9ff30
Alex <alexanderaljabari@gmx.com>
2019-04-06 12:29:28 +0300

    Added app files

---
+++ LiveChat/LiveChat/Controllers/LoginController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiveChat.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Autherize(LiveChat.user userModel)
+        {
+            using (userdbEntities db = new userdbEntities())
+            {
+                var userDetails = db.users.Where(x => x.userName == userModel.userName && x.userpassword == userModel.userpassword).FirstOrDefault();
+                if(userDetails==null)
+                {
+                    userModel.LoginErrorMessage= "Wrong Username or Password!";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["iduser"] = userDetails.iduser;
+                    return RedirectToAction("Index", "DataDB");
+                }
+            }
+            return View();
+        }
+
+        // GET: Login/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Login/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Login/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Login/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Login/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Login/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Login/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
