8f409c497e1b2b8f5edf1af5547524fdad02922e
Alex <alexanderaljabari@gmx.com>
2019-04-22 21:42:30 +0300

    fixed routing

---
+++ BlogNetApp/BlogNetApp/Controllers/HomePageController (2).cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BlogNetApp.DAL;
+using BlogNetApp.Models;
+
+namespace BlogNetApp.Controllers
+{
+    public class HomePageController : Controller
+    {
+
+
+        public string Title { get; set; }
+        private BlogContext db = new BlogContext();
+        public ActionResult Index(BlogNetApp.Models.User usermodel)
+        {
+
+
+                var posts = db.Posts.Include(p => p.Category).Include(p => p.User);
+            return View(posts.ToList());
+        }
+    }
+}
